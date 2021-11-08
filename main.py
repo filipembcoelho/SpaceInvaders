d6074f2f75e1a41d69f9380423cab1bffd7548f6
Filipe Coelho <filipemcoelho@gmail.com>
2021-11-08 21:32:37 +0000

    Insert the player image at a correct coordinate

--- main.py
+++ main.py
@@ -3,22 +3,35 @@ import pygame
 # Initialize pygame package (mandatory)
 pygame.init()
 
-# Create the screen using the set_mode with height x width
+# Create the screen using the set_mode with width x height
 screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption('Space Invaders')
 pygame.display.set_icon(pygame.image.load('images/ufo.png'))
 
+# Player
+playerImg = pygame.image.load('images/player.png')
+playerX = 368  # (800 / 2) - (64 / 2) = 368 (half of the screen)
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # listen for the QUIT event on the screen - infinite loop
 running = True
 while running:
+    # screen background with a RGB value
+    screen.fill((0, 128, 128))  # teal color
+
     # everything that happens inside the game window, should be inside this infinite loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # screen background with a RGB value
-    screen.fill((0, 128, 128))  # teal color
+    player()
     pygame.display.update()
+
 
