94ef2e829e8983e2126343f540a2dabe92e92cbd
Filipe Coelho <filipemcoelho@gmail.com>
2021-11-30 21:34:19 +0000

    Controlling the ship left and right and implement boundaries

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import pygame
 pygame.init()
 
 # Create the screen using the set_mode with width x height
-screen = pygame.display.set_mode((800, 600))
+screenWidth = 800
+screenHeight = 600
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 # Title and Icon
 pygame.display.set_caption('Space Invaders')
 pygame.display.set_icon(pygame.image.load('images/ufo.png'))
+shipWidth = 64
 
 # Player
 playerImg = pygame.image.load('images/player.png')
 playerX = 368  # (800 / 2) - (64 / 2) = 368 (half of the screen)
 playerY = 480
 playerX_change = 0
-playerY_change = 0
 
 speed = 0.1
+
+boundaryLeft = 0
+boundaryRight = 800
 
 
 def player(x, y):
@@ -40,17 +45,18 @@ while running:
                 playerX_change = -speed
             if event.key == pygame.K_RIGHT:
                 playerX_change = speed
-            if event.key == pygame.K_UP:
-                playerY_change = -speed
-            if event.key == pygame.K_DOWN:
-                playerY_change = speed
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
-                playerY_change = 0
 
     playerX += playerX_change
-    playerY += playerY_change
+
+    # boundary control for X axis
+    if playerX < boundaryLeft:
+        playerX = 0
+    elif playerX > boundaryRight - shipWidth:
+        playerX = boundaryRight - shipWidth
+
     player(playerX, playerY)
 
     pygame.display.update()
